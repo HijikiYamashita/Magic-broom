15d8f7f6b4b730b65f37eca61efda65d11d2751d
HijikiYamashita <godenter792@gmail.com>
2021-01-28 21:44:44 +0900

    へっどすぱ

---
+++ Assets/Scripts/AutoScroll.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoScroll : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        this.gameObject.transform.position += new Vector3(0, 0, 20 * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/WitchController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WitchController : MonoBehaviour
+{
+    [SerializeField] private int speed;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        transform.position += new Vector3(h * speed * Time.deltaTime, v * speed * Time.deltaTime, 0);
+    }
+}
