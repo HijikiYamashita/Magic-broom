6e1741e04cb12f4e5428b07027796ab0f34b7dac
HijikiYamashita <godenter792@gmail.com>
2021-01-31 10:03:00 +0900

    おーがにっく

--- Assets/Scripts/AutoScroll.cs
+++ Assets/Scripts/AutoScroll.cs
@@ -11,6 +11,6 @@ public class AutoScroll : MonoBehaviour
 
     void Update()
     {
-        this.gameObject.transform.position += new Vector3(0, 0, 20 * Time.deltaTime);
+        this.gameObject.transform.position += new Vector3(0, 0, 50 * Time.deltaTime);
     }
 }
